69f3457dd16a7b2c2229adb106217e5f049b51c9
enc-X <chodor+github@gmail.com>
2017-10-22 18:04:46 +0200

    homie 2.0.0-dev

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-[Homie](https://github.com/marvinroger/homie) based firmware for Sonoff wifi relay or any other ESP8266 based relay.
+[Homie](https://github.com/marvinroger/homie) based firmware for Sonoff wifi relay or any ESP8266 based relay.
 
 ## Features:
 * ON/OFF relay
 * Timer based relay
 * Configurable default (on boot) relay state - (MQTT independent)
 * Local button on/off
-* keepalive feature - device will reboot if not receive keepalive message in given time
+* Keepalive feature - device will reboot if not receive keepalive message in given time
+* Reverse mode - ON command means relay off, OFF command means relay on
+* Watchdog timer
 * All Homie buildin features (OTA,configuration)
+
+## Build and upload
+ * Attache module via USB-RS232 adapter
+ * Set proper serial port number in plantformio.ini file (upload_port variable)
+ * reboot module into program mode
+ * Flash module:
+   * execute <code>pio run --target upload --environment sonoff</code> for SONOFF or <code>pio run --target upload --environment generic</code> for generic ESP8266
+   * In Atom editor with PlatformIO prees F7 enter environment name (sonoff or generic) and choose <code>PIO uload</code> option from the list
 
 ## MQTT messages
 
@@ -36,22 +46,22 @@
   <td>Turn on relay for specific no. of seconds</td>
 </tr>
 <tr>
-  <td>_HOMIE_PREFIX_/_node-id_/keepalive/keepAliveValue</td>
+  <td>_HOMIE_PREFIX_/_node-id_/keepalive/timeOut</td>
   <td><code>\d+</code></td>
   <td>Device → Controller</td>
-  <td>Report of keepalive value (seconds), 0 - keep alive feature is not active</td>
+  <td>Report of keepalive timeout value (seconds), 0 - keep alive feature is not active</td>
 </tr>
 <tr>
-  <td>_HOMIE_PREFIX_/_node-id_/keepalive/keepAliveValue/set</td>
+  <td>_HOMIE_PREFIX_/_node-id_/keepalive/timeOut/set</td>
   <td><code>\d+</code></td>
   <td>Controller → Device</td>
-  <td>Set Report of keepalive time in seconds, 0 - keep alive feature is not active</td>
+  <td>Set Report of keepalive timeout in seconds, 0 - keep alive feature is not active</td>
 </tr>
 <tr>
   <td>_HOMIE_PREFIX_/_node-id_/keepalive/tick/set</td>
   <td><code>.*</code></td>
   <td>Controller → Device</td>
-  <td>Keepalive message from controller to gateway - if device will not receive during keepAliveValue time slot, it will reboot, keepalive is not active when keepAliveValue is equal 0</td>
+  <td>Keepalive message from controller to gateway - if device will not receive during keepAliveTimeOut time slot, it will reboot, keepalive is not active when keepAliveTimeOut is equal 0</td>
 </tr>
 <tr>
   <td>_HOMIE_PREFIX_/_node-id_/$online</td>
@@ -60,5 +70,3 @@
   <td><code>/true</code> when the device is online, <code>false</code> when the device is offline (through LWT)</td>
 </tr>
 </table>
-
-With PlatformIO  2 images 'sonoff' and 'myrelay' will be created.  
--- src/main.cpp
+++ src/main.cpp
@@ -16,23 +16,27 @@
 #define RELAY_OFF_STATE LOW
 #endif
 
-#ifdef MYRELAY
-// esp01 my relay with different pin configuration
+#ifdef GENERICRELAY
+// esp01 generic relay with different pin configuration
 #define PIN_RELAY 2
 #define PIN_BUTTON 0
+//#define PIN_LED 1
 #define RELAY_ON_STATE LOW
 #define RELAY_OFF_STATE HIGH
 #endif
 
 unsigned long downCounterStart;
 unsigned long downCounterLimit=0;
+unsigned long watchDogCounterStart=0;
 
 unsigned long keepAliveReceived=0;
 int lastButtonValue = 1;
 
 // EEPROM structure
 struct EEpromDataStruct {
-  int keepAliveValue; // 0 - disabled, keepalive time - seconds
+  int keepAliveTimeOut; // 0 - disabled, keepalive time - seconds
+  bool initialState;  // Initial state (just after boot - homie independet)
+  int watchDogTimeOut; // 0 - disabled, watchdog time limit - seconds
 };
 
 EEpromDataStruct EEpromData;
@@ -41,22 +45,69 @@ Bounce debouncerButton = Bounce();
 
 HomieNode relayNode("relay01", "relay");
 HomieNode keepAliveNode("keepalive", "keepalive");
+HomieNode watchDogNode("watchdog", "Watchdog mode");
 
-HomieSetting<bool> initialState("initialState", "Relay initial state");
+HomieSetting<bool> reverseMode("reverse mode", "Relay reverse mode");
 
+/*
+ * Recevied tick message for watchdog
+ */
+bool watchdogTickHandler(const HomieRange& range, const String& value)
+{
+  if (value == "0")
+  {
+    watchDogCounterStart = 0;
+  } else {
+    watchDogCounterStart = millis();
+  }
+  return true;
+}
+/*
+ * Received watchdog timeout value
+ */
+bool watchdogTimeOutHandler(const HomieRange& range, const String& value)
+{
+  int oldValue = EEpromData.watchDogTimeOut;
+  if (value.toInt() > 15)
+  {
+    EEpromData.watchDogTimeOut = value.toInt();
+  }
+  if (value=="0")
+  {
+    EEpromData.watchDogTimeOut = 0;
+  }
 
+  if (oldValue!=EEpromData.watchDogTimeOut)
+  {
+    String outMsg = String(EEpromData.watchDogTimeOut);
+    watchDogNode.setProperty("timeOut").send(outMsg);
+    EEPROM.put(0, EEpromData);
+    EEPROM.commit();
+  }
+}
 /*
  *
  */
-bool relayStateHandler(const HomieRange& range, const String& value) {
+bool relayStateHandler(const HomieRange& range, const String& value)
+{
   if (value == "ON") {
-    digitalWrite(PIN_RELAY, RELAY_ON_STATE);
+    if (reverseMode.get())
+    {
+      digitalWrite(PIN_RELAY, RELAY_OFF_STATE);
+    } else {
+      digitalWrite(PIN_RELAY, RELAY_ON_STATE);
+    }
     #ifdef PIN_LED
     digitalWrite(PIN_LED, LOW);
     #endif
     relayNode.setProperty("relayState").send("ON");
   } else if (value == "OFF") {
-    digitalWrite(PIN_RELAY, RELAY_OFF_STATE);
+    if (reverseMode.get())
+    {
+      digitalWrite(PIN_RELAY, RELAY_ON_STATE);
+    } else {
+      digitalWrite(PIN_RELAY, RELAY_OFF_STATE);
+    }
     #ifdef PIN_LED
     digitalWrite(PIN_LED, HIGH);
     #endif
@@ -79,19 +130,21 @@ bool keepAliveTickHandler(HomieRange range, String value)
 /*
  * Keepalive message handler
  */
-bool keepAliveValueHandler(HomieRange range, String value)
+bool keepAliveTimeOutHandler(HomieRange range, String value)
 {
-  int oldValue = EEpromData.keepAliveValue;
+  int oldValue = EEpromData.keepAliveTimeOut;
   if (value.toInt() > 0)
   {
-    EEpromData.keepAliveValue = value.toInt();
+    EEpromData.keepAliveTimeOut = value.toInt();
   }
   if (value=="0")
   {
-    EEpromData.keepAliveValue = 0;
+    EEpromData.keepAliveTimeOut = 0;
   }
-  if (oldValue!=EEpromData.keepAliveValue)
+  if (oldValue!=EEpromData.keepAliveTimeOut)
   {
+    String outMsg = String(EEpromData.keepAliveTimeOut);
+    keepAliveNode.setProperty("timeOut").send(outMsg);
     EEPROM.put(0, EEpromData);
     EEPROM.commit();
   }
@@ -104,7 +157,12 @@ bool relayTimerHandler(HomieRange range, String value)
 {
   if (value.toInt() > 0)
   {
-    digitalWrite(PIN_RELAY, RELAY_ON_STATE);
+    if (reverseMode.get())
+    {
+      digitalWrite(PIN_RELAY, RELAY_OFF_STATE);
+    } else {
+      digitalWrite(PIN_RELAY, RELAY_ON_STATE);
+    }
     #ifdef PIN_LED
     digitalWrite(PIN_LED, LOW);
     #endif
@@ -118,23 +176,56 @@ bool relayTimerHandler(HomieRange range, String value)
   }
 }
 
-
+/*
+ * Initial mode handler
+ */
+bool relayInitModeHandler(HomieRange range, String value)
+{
+  int oldValue = EEpromData.initialState;
+  if (value.toInt() == 1 or value=="ON")
+  {
+    relayNode.setProperty("relayInitMode").send("1");
+    EEpromData.initialState=1;
+  } else {
+    relayNode.setProperty("relayInitMode").send( "0");
+    EEpromData.initialState=0;
+  }
+  if (oldValue!=EEpromData.initialState)
+  {
+    EEPROM.put(0, EEpromData);
+    EEPROM.commit();
+  }
+  return true;
+}
 
 /*
  * Homie setup handler
  */
 void setupHandler()
 {
-  if (initialState.get())
+  HomieRange emptyRange;
+  if (EEpromData.initialState)
   {
-    relayNode.setProperty("relayState").send("ON");
+    if (reverseMode.get())
+      relayStateHandler(emptyRange, "OFF");
+    else
+      relayStateHandler(emptyRange, "ON");
+    relayNode.setProperty("relayInitMode").send("1");
   } else {
-    relayNode.setProperty("relayState").send("OFF");
+    if (reverseMode.get())
+      relayStateHandler(emptyRange, "ON");
+    else
+      relayStateHandler(emptyRange, "OFF");
+
+    relayNode.setProperty("relayInitMode").send("0");
   }
-  String outMsg = String(EEpromData.keepAliveValue);
-  keepAliveNode.setProperty("keepAliveValue").send(outMsg);
+  String outMsg = String(EEpromData.keepAliveTimeOut);
+  keepAliveNode.setProperty("timeOut").send(outMsg);
+  outMsg = EEpromData.watchDogTimeOut;
+  watchDogNode.setProperty("timeOut").send(outMsg);
   keepAliveReceived=millis();
 }
+
 
 /*
  * Homie loop handler
@@ -146,7 +237,12 @@ void loopHandler()
     if ((millis() - downCounterStart ) > downCounterLimit)
     {
       // Turn off relay
-      digitalWrite(PIN_RELAY, RELAY_OFF_STATE);
+      if (reverseMode.get())
+      {
+        digitalWrite(PIN_RELAY, RELAY_ON_STATE);
+      } else {
+        digitalWrite(PIN_RELAY, RELAY_OFF_STATE);
+      }
       #ifdef PIN_LED
       digitalWrite(PIN_LED, HIGH);
       #endif
@@ -164,7 +260,9 @@ void loopHandler()
     if (buttonValue == HIGH)
     {
       HomieRange emptyRange;
-      if (relayValue == RELAY_ON_STATE)
+      if (
+          ((!reverseMode.get()) && relayValue == RELAY_ON_STATE) ||
+          (( reverseMode.get()) && relayValue == RELAY_OFF_STATE) )
       {
         relayStateHandler(emptyRange, "OFF");
       } else {
@@ -175,9 +273,15 @@ void loopHandler()
   debouncerButton.update();
 
   // Check if keepalive is supported and expired
-  if (EEpromData.keepAliveValue != 0 && (millis() - keepAliveReceived) > EEpromData.keepAliveValue*1000 )
+  if (EEpromData.keepAliveTimeOut != 0 && (millis() - keepAliveReceived) > EEpromData.keepAliveTimeOut*1000 )
   {
     ESP.restart();
+  }
+  if (watchDogCounterStart!=0 && EEpromData.watchDogTimeOut!=0 && (millis() - watchDogCounterStart) > EEpromData.watchDogTimeOut * 1000 )
+  {
+    HomieRange emptyRange;
+    relayTimerHandler(emptyRange, "10"); // Disable relay for 10 sec
+    watchDogCounterStart = millis();
   }
 }
 
@@ -188,7 +292,9 @@ void onHomieEvent(const HomieEvent& event) {
   switch(event.type) {
     case HomieEventType::CONFIGURATION_MODE: // Default eeprom data in configuration mode
       digitalWrite(PIN_RELAY, RELAY_OFF_STATE);
-      EEpromData.keepAliveValue = 0;
+      EEpromData.keepAliveTimeOut = 0;
+      EEpromData.initialState = false;
+      EEpromData.watchDogTimeOut = 0;
       EEPROM.put(0, EEpromData);
       EEPROM.commit();
       break;
@@ -208,9 +314,6 @@ void onHomieEvent(const HomieEvent& event) {
     case HomieEventType::WIFI_DISCONNECTED:
       // Do whatever you want when Wi-Fi is disconnected in normal mode
       break;
-    case HomieEventType::MQTT_CONNECTED:
-      // Do whatever you want when MQTT is connected in normal mode
-      break;
     case HomieEventType::MQTT_DISCONNECTED:
       // Do whatever you want when MQTT is disconnected in normal mode
       break;
@@ -222,9 +325,12 @@ void onHomieEvent(const HomieEvent& event) {
  */
 void setup()
 {
+  #ifdef SONOFF
+  Serial.begin(115200);
+  Serial.println("\n\n");
+  #endif
   EEPROM.begin(sizeof(EEpromData));
   EEPROM.get(0,EEpromData);
-
 
   pinMode(PIN_RELAY, OUTPUT);
   #ifdef PIN_LED
@@ -235,18 +341,20 @@ void setup()
 	debouncerButton.attach(PIN_BUTTON);
 	debouncerButton.interval(50);
 
-  if (initialState.get())
+  if (EEpromData.initialState)
   {
-    digitalWrite(PIN_RELAY, RELAY_ON_STATE);
-    #ifdef PIN_LED
-    digitalWrite(PIN_LED, LOW);
+    #ifdef SONOFF
+    Serial.println( "Sonoff ON - initial state\n");
     #endif
+    digitalWrite(PIN_RELAY,RELAY_ON_STATE);
   } else {
-    digitalWrite(PIN_RELAY, RELAY_OFF_STATE);
-    #ifdef PIN_LED
-    digitalWrite(PIN_LED, HIGH);
+    #ifdef SONOFF
+    Serial.println( "Sonoff OFF - initial state\n");
     #endif
+    digitalWrite(PIN_RELAY,RELAY_OFF_STATE);
+    EEpromData.initialState=0;
   }
+
 
   Homie_setFirmware(FIRMWARE_NAME, FIRMWARE_VER);
   Homie.setSetupFunction(setupHandler);
@@ -255,15 +363,20 @@ void setup()
   Homie.setLedPin(PIN_LED, LOW);
   #else
   Homie.disableLedFeedback();
+  #endif
+  #ifdef GENERICRELAY
   Homie.disableLogging();
   #endif
   Homie.setResetTrigger(PIN_BUTTON, LOW, 10000);
   relayNode.advertise("relayState").settable(relayStateHandler);
   relayNode.advertise("relayTimer").settable(relayTimerHandler);
+  relayNode.advertise("relayInitMode").settable(relayInitModeHandler);
+  watchDogNode.advertise("tick").settable(watchdogTickHandler);
+  watchDogNode.advertise("timeOut").settable(watchdogTimeOutHandler);
   keepAliveNode.advertise("tick").settable(keepAliveTickHandler);
-  keepAliveNode.advertise("keepAliveValue").settable(keepAliveValueHandler);
+  keepAliveNode.advertise("timeOut").settable(keepAliveTimeOutHandler);
   Homie.onEvent(onHomieEvent);
-  initialState.setDefaultValue(false).setValidator([] (bool candidate) {
+  reverseMode.setDefaultValue(false).setValidator([] (bool candidate) {
     return (candidate >= 0) && (candidate <= 1);
   });
   Homie.setup();
